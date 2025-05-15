bc4ad370a685d728611d9fd9a70e77b23900eeeb
César García <ceegee@outlook.com>
2025-05-15 14:27:14 -0700

    Add project files.

---
+++ 1. Two Sum/py/1.TwoSum.py
@@ -0,0 +1,25 @@
+# LeetCode challenge: 1. Two Sum: https://leetcode.com/problems/two-sum/description/
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+import unittest
+
+class twoSum_Tests(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    def test1(self):
+        self.assertEqual(self.solution.twoSum([2,7,11,15], 9), [0,1])
+    def test2(self):
+        self.assertEqual(self.solution.twoSum([3,2,4], 6), [1,2])
+    def test3(self):
+        self.assertEqual(self.solution.twoSum([3,3], 6), [0,1])
+
+if __name__ == "__main__":
+    unittest.main()
