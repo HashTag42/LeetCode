79e599614b99a0a81f486e35f3d08fc79fd97bea
César García <ceegee@outlook.com>
2025-05-15 15:15:00 -0700

    Update TwoSum.py with tests and URL formatting
    Reformatted the LeetCode challenge URL to use angle brackets.
    Moved the `unittest` import to a new line and added a
    `twoSum_Tests` class for unit testing the `Solution` class.
    Implemented a `setUp` method and a test case to validate
    the `twoSum` method's functionality.

--- 1. Two Sum/py/1.TwoSum.py
+++ 1. Two Sum/py/1.TwoSum.py
@@ -1,6 +1,7 @@
-# LeetCode challenge: 1. Two Sum: https://leetcode.com/problems/two-sum/description/
+# LeetCode challenge: 1. Two Sum: <https://leetcode.com/problems/two-sum/description/>
 
 from typing import List
+import unittest
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -8,8 +9,6 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
-
-import unittest
 
 class twoSum_Tests(unittest.TestCase):
     def setUp(self):
