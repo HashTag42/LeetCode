ca6beba56febb6e0831942ea387eba67002e8ca9
César García <ceegee@outlook.com>
2025-05-15 15:19:26 -0700

    Add project for 88. Merge Sorted Array problem
    This commit introduces a new project for the "88. Merge Sorted Array" problem, updating the solution file (`LeetCode.sln`) and creating a Python project file (`88. Merge Sorted Array (Python).pyproj`). A new Python script (`88.MergeSortedArray.py`) is added, which includes the solution implementation and unit tests for validation.

---
+++ 88. Merge Sorted Array/py/88.MergeSortedArray.py
@@ -0,0 +1,32 @@
+# LeetCode challenge: 88. Merge Sorted Array <https://leetcode.com/problems/merge-sorted-array/description/>
+
+from typing import List
+import unittest
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m, m+n):
+             nums1[i] = nums2[i-n]
+        nums1.sort()
+
+class Solution_Tests(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    def test1(self):
+        nums1 = [1,2,3,0,0,0]
+        self.solution.merge(nums1, m = 3, nums2 = [2,5,6], n = 3)
+        self.assertEqual(nums1, [1,2,2,3,5,6])
+    def test2(self):
+        nums1 = [1]
+        self.solution.merge(nums1, m = 1, nums2 = [], n = 0)
+        self.assertEqual(nums1, [1])
+    def test3(self):
+        nums1 = [0]
+        self.solution.merge(nums1, m = 0, nums2 = [1], n = 1)
+        self.assertEqual(nums1, [1])
+
+if __name__ == "__main__":
+    unittest.main()
