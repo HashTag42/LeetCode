7bc2a961550e3cf7bbf1ce5b049c58c10dc9fa5c
César García <ceegee@outlook.com>
2025-05-24 11:34:42 -0700

    Refactor maxProfit solution and enhance unit tests
    Replaced the nested loop approach with a single-pass algorithm to calculate maximum profit for the "Best Time to Buy and Sell Stock" problem. Reduce time complexity from O(N^2) to O(N). Added multiple unit tests to cover various scenarios and edge cases, ensuring the solution's correctness.

--- 121. Best Time to Buy and Sell Stock/py/121.BestTimetoBuyandSellStock.py
+++ 121. Best Time to Buy and Sell Stock/py/121.BestTimetoBuyandSellStock.py
@@ -1,24 +1,68 @@
 # LeetCode challenge: 121. Best Time to Buy and Sell Stock: <https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/>
 
+################################################################################
 from typing import List
+from math import inf
 import unittest
+################################################################################
 
+################################################################################
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxp = 0
+        min_price  =  inf
+        max_price  = -inf
+        cur_profit = -inf
+        max_profit = -inf
         for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
-                maxp = max(maxp, prices[j] - prices[i])
-        return maxp
+            price = prices[i]
+            if price < min_price:
+                min_price = price
+                max_price = -inf
+            if price > max_price:
+                max_price = price
+                cur_profit = max_price - min_price
+                max_profit = max(cur_profit, max_profit)
+        return max_profit
+################################################################################
 
+################################################################################
 class maxProfit_Tests(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
-    def test1(self):
+    def test01(self):
+        self.assertEqual(self.solution.maxProfit([0]), 0)
+    def test02(self):
+        self.assertEqual(self.solution.maxProfit([0,0]), 0)
+    def test03(self):
+        self.assertEqual(self.solution.maxProfit([0,1]), 1)
+    def test04(self):
+        self.assertEqual(self.solution.maxProfit([1,0]), 0)
+    def test05(self):
+        self.assertEqual(self.solution.maxProfit([0,1,2]), 2)
+    def test06(self):
+        self.assertEqual(self.solution.maxProfit([0,2,1]), 2)
+    def test07(self):
         self.assertEqual(self.solution.maxProfit([7,1,5,3,6,4]), 5)
-    def test2(self):
+    def test08(self):
         self.assertEqual(self.solution.maxProfit([7,6,4,3,1]), 0)
+    def test09(self):
+        self.assertEqual(self.solution.maxProfit([0,2,1,4]), 4)
+    def test10(self):
+        self.assertEqual(self.solution.maxProfit([0,1,2,3,4,5]), 5)
+    def test11(self):
+        self.assertEqual(self.solution.maxProfit([2,4,1,4]), 3)
+    def test12(self):
+        self.assertEqual(self.solution.maxProfit([2,4,1]), 2)
+    def test13(self):
+        self.assertEqual(self.solution.maxProfit([2,5,1,3]), 3)
+    def test14(self):
+        self.assertEqual(self.solution.maxProfit([3,2,6,5,0,3]), 4)
+    def tearDown(self):
+        pass
+################################################################################
 
+################################################################################
 if __name__ == "__main__":
     unittest.main()
+################################################################################
 
