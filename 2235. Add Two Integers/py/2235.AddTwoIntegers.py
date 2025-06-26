12cd3c347739dd66300a53491b7698f54b010caa
César García <ceegee@outlook.com>
2025-06-26 12:26:27 -0700

    Add project for 2235. Add Two Integers (Python)
    - Created a new project file for "2235. Add Two Integers (Python)" with Visual Studio configurations.
    - Implemented the solution for the LeetCode challenge, including a `Solution` class and unit tests using `unittest`.
    - Updated the `LeetCode.sln` file to include the new project and adjusted global sections for proper configuration in Debug and Release modes.

---
+++ 2235. Add Two Integers/py/2235.AddTwoIntegers.py
@@ -0,0 +1,18 @@
+# LeetCode challenge: 2235. Add Two Integers: <https://leetcode.com/problems/add-two-integers/description/>
+
+import unittest
+
+class Solution:
+    def mySum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+class sum_tests(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+    def test1(self):
+        self.assertEqual(self.solution.mySum(12, 5), 17)
+    def test2(self):
+        self.assertEqual(self.solution.mySum(-10, 4), -6)
+
+if __name__ == "__main__":
+    unittest.main()
