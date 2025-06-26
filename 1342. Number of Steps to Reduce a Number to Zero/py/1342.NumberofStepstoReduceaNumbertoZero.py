3c0988467ac5bcb03d47422e4fe66ffc6a8d79bc
César García <ceegee@outlook.com>
2025-06-26 14:01:03 -0700

    Refactor imports in 1342.NumberofStepstoReduceaNumbertoZero.py
    Removed unused `unittest` import and updated `tkinter` import to only include `N`. The core functionality of the `Solution` class and its method `numberOfSteps` remain unchanged.

--- 1342. Number of Steps to Reduce a Number to Zero/py/1342.NumberofStepstoReduceaNumbertoZero.py
+++ 1342. Number of Steps to Reduce a Number to Zero/py/1342.NumberofStepstoReduceaNumbertoZero.py
@@ -1,6 +1,5 @@
 # LeetCode challenge: 1342. Number of Steps to Reduce a Number to Zero: <https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/>
 
-from tkinter import N
 import unittest
 
 class Solution:
