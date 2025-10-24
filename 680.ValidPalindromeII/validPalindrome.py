bc14b896d8b5087ca0de6f0ced5bfe310057b434
César García <ceegee@outlook.com>
2025-10-24 16:42:53 -0700

    Removing unnecesary check.

--- 680.ValidPalindromeII/validPalindrome.py
+++ 680.ValidPalindromeII/validPalindrome.py
@@ -4,9 +4,11 @@
 
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if s == s[::-1]:
-            return True
-
+        """
+        Returns true if the str s can be a palindrome by removing 0 or 1 characters from it. False otherwise.
+        Time complexity: O(n^2)
+        Space complexity: O(n)
+        """
         for i in range(len(s)):
             copy = s[:i] + s[i + 1:]
             if copy == copy[::-1]:
