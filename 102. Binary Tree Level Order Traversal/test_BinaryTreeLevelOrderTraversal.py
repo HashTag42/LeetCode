316f83c32467b993b3a8cd57fdaf210108ce691d
César García <ceegee@outlook.com>
2025-10-24 21:23:25 -0700

    Refactor test cases

--- 102. Binary Tree Level Order Traversal/test_BinaryTreeLevelOrderTraversal.py
+++ 102. Binary Tree Level Order Traversal/test_BinaryTreeLevelOrderTraversal.py
@@ -1,15 +1,29 @@
-import pytest
-from BinaryTreeLevelOrderTraversal import Solution
+from BinaryTreeLevelOrderTraversal import Solution, TreeNode
 
 
-cases = [
-    ([], []),
-    # ([1], [[1]]),
-    # ([3, 9, 20, None, None, 15, 7], [[3], [9, 20], [15, 7]]),
-]
-
-
-@pytest.mark.parametrize("tree,expected", cases)
-def test__Solution__levelOrder(tree, expected):
+def test__Solution__levelOrder__case0():
     solution = Solution()
-    assert expected == solution.levelOrder(tree)
+    root = None
+    assert solution.levelOrder(root) == []
+
+
+def test__Solution__levelOrder__case1():
+    solution = Solution()
+    root = TreeNode()
+    assert solution.levelOrder(root) == [[0]]
+
+
+def test__Solution__levelOrder__case2():
+    solution = Solution()
+    root = TreeNode(1)
+    assert solution.levelOrder(root) == [[1]]
+
+
+def test__Solution__levelOrder__case3():
+    solution = Solution()
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    assert solution.levelOrder(root) == [[3], [9, 20], [15, 7]]
