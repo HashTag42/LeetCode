ed807d4a8b6b51c1c7715ed070fb7ad07b918c7b
César García <ceegee@outlook.com>
2025-10-26 17:21:21 -0700

    Add two-pointer implementations

--- 125. Valid Palindrome/isPalindrome.py
+++ 125. Valid Palindrome/isPalindrome.py
@@ -5,12 +5,47 @@ https://leetcode.com/problems/valid-palindrome/
 
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome1(self, s: str) -> bool:
         """
         Returns true if a string is a palindrome. False otherwise.
-        Only considers alphanumeric characters. Character case is ignored.
+        Only considers alphanumeric characters in the string. Character case is ignored.
         Time Complexity: O(n)
         Space Complexity: O(n)
         """
         cleaned = ''.join(c.lower() for c in s if c.isalnum())
         return cleaned == cleaned[::-1]
+
+    def isPalindrome2(self, s: str) -> bool:
+        """
+        Returns true if a string is a palindrome. False otherwise.
+        Only considers alphanumeric characters in the string. Character case is ignored.
+        Two-pointer implementation.
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        cleaned = ''.join(c.lower() for c in s if c.isalnum())
+        for i in range(len(cleaned) // 2):
+            if cleaned[i] != cleaned[len(cleaned) - i - 1]:
+                return False
+        return True
+
+    def isPalindrome3(self, s: str) -> bool:
+        """
+        Returns true if a string is a palindrome. False otherwise.
+        Only considers alphanumeric characters in the string. Character case is ignored.
+        Two-pointer implementation.
+        Does not make a copy of the source string.
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
--- 125. Valid Palindrome/test_isPalindrone.py
+++ 125. Valid Palindrome/test_isPalindrone.py
@@ -5,13 +5,29 @@ import pytest
 cases = [
     ("", True),
     (" ", True),
+    ("a", True),
     ("A man, a plan, a canal: Panama", True),
     ("race a car", False),
     ("0P", False),
+    ("abba", True),
+    ("abcba", True),
+    ("abcabc", False),
 ]
 
 
 @pytest.mark.parametrize("s, expected", cases)
-def test__isPalindrome__(s, expected):
+def test__isPalindrome1__(s, expected):
     solution = Solution()
-    assert solution.isPalindrome(s) == expected
+    assert solution.isPalindrome1(s) == expected
+
+
+@pytest.mark.parametrize("s, expected", cases)
+def test__isPalindrome2__(s, expected):
+    solution = Solution()
+    assert solution.isPalindrome2(s) == expected
+
+
+@pytest.mark.parametrize("s, expected", cases)
+def test__isPalindrome3__(s, expected):
+    solution = Solution()
+    assert solution.isPalindrome3(s) == expected
