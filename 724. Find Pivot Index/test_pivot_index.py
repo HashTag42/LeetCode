e3e7f663efbe6f709408432f1226b72a33a0a457
César García <ceegee@outlook.com>
2026-01-20 17:50:10 -0800

    Add solution to LeetCode problem 724. Find Pivot Index

---
+++ 724. Find Pivot Index/test_pivot_index.py
@@ -0,0 +1,15 @@
+from pivot_index import pivot_index
+import pytest
+
+
+test_cases = [
+    # (nums, expected)
+    ([1, 7, 3, 6, 5, 6], 3),
+    ([1, 2, 3], -1),
+    ([2, 1, -1], -0),
+]
+
+
+@pytest.mark.parametrize('nums, expected', test_cases)
+def test_pivot_index(nums, expected):
+    assert pivot_index(nums) == expected
