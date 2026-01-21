273b2851c1e0b565334787231f36f624104cb9a2
César García <ceegee@outlook.com>
2026-01-20 19:20:11 -0800

    Add solution to LeetCode problem 1207. Unique Number of Occurrences

---
+++ 1207. Unique Number of Occurrences/test_unique_occurrences.py
@@ -0,0 +1,15 @@
+from unique_occurrences import unique_occurrences
+import pytest
+
+
+test_cases = [
+    # (arr, expected)
+    ([1, 2, 2, 1, 1, 3], True),
+    ([1, 2], False),
+    ([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0], True),
+]
+
+
+@pytest.mark.parametrize('arr, expected', test_cases)
+def test_unique_occurrences(arr, expected):
+    assert unique_occurrences(arr) == expected
