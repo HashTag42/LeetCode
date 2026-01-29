4bae2a59f31f174f410f33f8f16822452bc39445
César García <ceegee@outlook.com>
2026-01-28 17:22:11 -0800

    Refactor after code review

--- 933. Number of Recent Calls/py/test_RecentCounter.py
+++ 933. Number of Recent Calls/py/test_RecentCounter.py
@@ -1,17 +1,11 @@
 from RecentCounter import RecentCounter
 
-input_a = ["RecentCounter", "ping", "ping", "ping", "ping"]
-input_b = [[], [1], [100], [3001], [3002]]
-expected = [None, 1, 2, 3, 3]
 
-
-def test_RecentCounter():
+def test_RecentCounter_example():
     rc = RecentCounter()
-    result = []
-    for i, a in enumerate(input_a):
-        match a:
-            case "RecentCounter":
-                result.append(None)
-            case "ping":
-                result.append(rc.ping(input_b[i][0]))
+    inputs = [1, 100, 3001, 3002]
+    expected = [1, 2, 3, 3]
+
+    result = [rc.ping(t) for t in inputs]
+
     assert result == expected
