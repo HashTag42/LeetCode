3985f16da86fb68443834d92d56c828e1f40978a
César García <ceegee@outlook.com>
2026-01-29 15:00:40 -0800

    Add Python solution to LeetCode problem 1137. N-th Tribonacci Number

---
+++ 1137. N-th Tribonacci Number/py/test_tribonacci.py
@@ -0,0 +1,15 @@
+import json
+import pathlib
+import pytest
+from tribonacci import tribonacci
+
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
+
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
+
+
+@pytest.mark.parametrize('num, expected', test_cases)
+def test_tribonacci(num, expected):
+    assert tribonacci(num) == expected
