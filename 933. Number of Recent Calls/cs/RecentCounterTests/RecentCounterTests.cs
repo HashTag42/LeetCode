3ff21f4b295e55f5a87e9f2ada35c8a1a28fe326
César García <ceegee@outlook.com>
2026-01-29 00:18:39 -0800

    Move test case data to a JSON file

--- 933. Number of Recent Calls/cs/RecentCounterTests/RecentCounterTests.cs
+++ 933. Number of Recent Calls/cs/RecentCounterTests/RecentCounterTests.cs
@@ -1,15 +1,35 @@
+using System.Text.Json;
+using Shared;
 using Xunit;
 
 namespace LeetCode.RecentCounter;
 
 public class RecentCounterTests
 {
-    [Fact]
-    public void TestRecentCounter()
+    public static TheoryData<int[], int[]> TestCases
     {
-        List<int> inputs = [1, 100, 3001, 3002];
-        List<int> expected = [1, 2, 3, 3];
+        get
+        {
+            var data = new TheoryData<int[], int[]>();
+            var rows = TestDataLoader.LoadNestedArrayData("test_cases.json");
+            foreach (var row in rows)
+            {
+                var inputs = ((JsonElement)row[0]).EnumerateArray()
+                    .Select(e => e.GetInt32())
+                    .ToArray();
+                var expected = ((JsonElement)row[1]).EnumerateArray()
+                    .Select(e => e.GetInt32())
+                    .ToArray();
+                data.Add(inputs, expected);
+            }
+            return data;
+        }
+    }
 
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void TestRecentCounter(int[] inputs, int[] expected)
+    {
         var counter = new RecentCounter();
         List<int> result = [];
         foreach (var t in inputs)
--- 933. Number of Recent Calls/py/test_RecentCounter.py
+++ 933. Number of Recent Calls/py/test_RecentCounter.py
@@ -1,11 +1,17 @@
+import json
+import pathlib
+import pytest
 from RecentCounter import RecentCounter
 
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
 
-def test_RecentCounter_example():
-    inputs = [1, 100, 3001, 3002]
-    expected = [1, 2, 3, 3]
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
 
+
+@pytest.mark.parametrize('inputs, expected', test_cases)
+def test_RecentCounter_example(inputs, expected):
     rc = RecentCounter()
     result = [rc.ping(t) for t in inputs]
-
     assert result == expected
