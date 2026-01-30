a113c1d46743430cf3d07c1ec544e386e7326af6
César García <ceegee@outlook.com>
2026-01-29 22:33:00 -0800

    Refactor expressions for clarity

--- 746. Min Cost Climbing Stairs/py/minCostClimbingStairs.py
+++ 746. Min Cost Climbing Stairs/py/minCostClimbingStairs.py
@@ -49,6 +49,7 @@ def minCostClimbingStairs(cost: list[int]) -> int:
 
     for i in range(2, len(cost)):
         current = cost[i] + min(prev1, prev2)
-        prev2, prev1 = prev1, current
+        prev2 = prev1
+        prev1 = current
 
     return min(prev1, prev2)
