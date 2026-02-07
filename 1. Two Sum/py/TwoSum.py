c8cdb5bd0599006cbc5ad55beee471a88ca5e504
César García <ceegee@outlook.com>
2026-02-06 22:14:03 -0800

    Fix linter issue

--- 1. Two Sum/py/TwoSum.py
+++ 1. Two Sum/py/TwoSum.py
@@ -1,15 +1,13 @@
 # LeetCode challenge: 1. Two Sum: <https://leetcode.com/problems/two-sum/description/>
 
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if nums == [] or nums is None:
-            length = 0
-        else:
-            length = len(nums)
-        for i in range(length):
+    def twoSum(self, nums: Optional[List[int]], target: int) -> Optional[List[int]]:
+        if nums is None or len(nums) < 2:
+            return None
+        for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
