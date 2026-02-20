adbdbba869a3583362a3bde35cd8a41b36d0e388
César García <ceegee@outlook.com>
2026-02-19 18:44:13 -0800

    Update with code review feedback

--- 9. Palindrome Number/py/PalindromeNumber.py
+++ 9. Palindrome Number/py/PalindromeNumber.py
@@ -4,10 +4,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         # Time Complexity = O(N)
-        # Space Complexity = O(1)
+        # Space Complexity = O(N)
         strX = str(x)
         lenX = len(strX)
-        for i in range(lenX - 1):
+        for i in range(lenX // 2):
             if strX[i] != strX[lenX - 1 - i]:
                 return False
         return True
