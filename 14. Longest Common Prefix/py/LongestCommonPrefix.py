579069796d61c2a8132b98a12810d5d1bd04a77b
César García <ceegee@outlook.com>
2026-02-24 21:03:43 -0800

    Refactor with code review feedback

--- 14. Longest Common Prefix/py/LongestCommonPrefix.py
+++ 14. Longest Common Prefix/py/LongestCommonPrefix.py
@@ -2,23 +2,12 @@
 # <https://leetcode.com/problems/longest-common-prefix/description/>
 
 class Solution:
-
-    ''' Find the longest common prefix string among an array of strings.
-        If there is no common prefix, return an empty string"". '''
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        if len(strs) == 0:
+        ''' Find the longest common prefix string among an array of strings.
+            If there is no common prefix, return an empty string"". '''
+        if not strs:
             return ""
-        if len(strs) == 1:
-            return strs[0]
-        common_prefix = ""
-        i = 0
-        while (True):
-            if i >= len(strs[0]):
-                break
-            char = strs[0][i]
-            for s in strs:
-                if i >= len(s) or char != s[i]:
-                    return common_prefix
-            common_prefix += char
-            i += 1
-        return common_prefix
+        for i, chars in enumerate(zip(*strs)):
+            if len(set(chars)) > 1:
+                return strs[0][:i]
+        return min(strs)  # All chars matched up to shortest string
