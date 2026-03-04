16af8df49728cb15b15d4d4243689da9b8b730e7
César García <ceegee@outlook.com>
2026-03-03 18:06:51 -0800

    Add C# solution to LeetCode problem: 26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/cs/RemoveDuplicates/RemoveDuplicates.cs
@@ -0,0 +1,27 @@
+// LeetCode problem: 26. Remove Duplicates from Sorted Array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+namespace LeetCode.RemoveDuplicates;
+
+public class Solution
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        int insertIndex = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] != nums[i])
+            {
+                nums[insertIndex] = nums[i];
+                insertIndex += 1;
+            }
+        }
+
+        return insertIndex;
+    }
+}
---
+++ 26. Remove Duplicates from Sorted Array/cs/RemoveDuplicatesTests/RemoveDuplicatesTests.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using Shared;
+using Xunit;
+
+namespace LeetCode.RemoveDuplicates;
+
+public class RemoveDuplicatesTests
+{
+    [Fact]
+    public void TestCases_LoadsSuccessfully()
+    {
+        Assert.NotEmpty(TestCases);
+    }
+
+    public static TheoryData<int[]?, int> TestCases
+    {
+        get
+        {
+            var data = new TheoryData<int[]?, int>();
+            var rows = TestDataLoader.LoadNestedArrayData("test_cases.json");
+            foreach (var row in rows)
+            {
+                var list = row[0] is JsonElement listEl
+                    ? listEl.EnumerateArray().Select(e => e.GetInt32()).ToArray()
+                    : null;
+                var expected = ((JsonElement)row[1]).GetInt32();
+                data.Add(list, expected);
+            }
+            return data;
+        }
+    }
+
+    [Theory(DisplayName = "RemoveDuplicates with varied inputs")]
+    [MemberData(nameof(TestCases))]
+    public void TestRemoveDuplicates(int[]? list, int expected)
+    {
+        if (list is null)
+            return;
+        Assert.Equal(expected, Solution.RemoveDuplicates(list));
+    }
+}
