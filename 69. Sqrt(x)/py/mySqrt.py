a39541872649cab505a8f60bcf51dd37f1271ba5
César García <ceegee@outlook.com>
2026-03-11 13:36:03 -0700

    Refactor after code review

--- 69. Sqrt(x)/py/mySqrt.py
+++ 69. Sqrt(x)/py/mySqrt.py
@@ -1,12 +1,16 @@
-# Leet Code challenge: 69. Sqrt(x)
-# <https://leetcode.com/problems/sqrtx/>
+# Leet Code problem: 69. Sqrt(x)
+# https://leetcode.com/problems/sqrtx/
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        previous, current = 0, 0
-        while True:
-            previous = current
-            current += 1
-            sum = current * current
-            if sum > x:
-                return previous
+        lo, hi = 0, x
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return hi
