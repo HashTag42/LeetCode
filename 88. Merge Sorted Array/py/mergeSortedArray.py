25b9f5d8648875f28796981ccba206bd359d6eb5
César García <ceegee@outlook.com>
2026-03-13 22:25:54 -0700

    Refactor after code review

--- 88. Merge Sorted Array/py/mergeSortedArray.py
+++ 88. Merge Sorted Array/py/mergeSortedArray.py
@@ -4,10 +4,18 @@
 
 class Solution:
     def mergeSortedArray(self, nums1: list[int], m: int, nums2: list[int], n: int) -> list[int]:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        for i in range(m, m + n):
-            nums1[i] = nums2[i - m]
-        nums1.sort()
+        '''
+        Complexity:
+            Time: O(m + n)
+            Space: O(1)
+        '''
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
         return nums1
