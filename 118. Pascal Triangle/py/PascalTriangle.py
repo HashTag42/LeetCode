52de4564c2de96f9b3e9408771aa5b41a876e4dd
César García <ceegee@outlook.com>
2026-03-17 19:42:39 -0700

    Refactor after code review

--- 118. Pascal Triangle/py/PascalTriangle.py
+++ 118. Pascal Triangle/py/PascalTriangle.py
@@ -6,23 +6,8 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        tree = []
-        if numRows == 0:
-            return tree
-        if numRows == 1:
-            tree.append([1])
-            return tree
-        if numRows == 2:
-            tree.append([1])
-            tree.append([1, 1])
-            return tree
-
-        tree.append([1])
-        tree.append([1, 1])
-        for i in range(2, numRows):
-            row = [1]
-            for j in range(1, i):
-                row.append(tree[i - 1][j - 1] + tree[i - 1][j])
-            row.append(1)
+        tree = [[1]]
+        for i in range(1, numRows):
+            row = [1] + [tree[i-1][j-1] + tree[i-1][j] for j in range(1, i)] + [1]
             tree.append(row)
         return tree
