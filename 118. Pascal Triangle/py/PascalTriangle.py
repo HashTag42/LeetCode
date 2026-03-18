aa9262706e833002eae42078069b632d6fa5969e
César García <ceegee@outlook.com>
2026-03-17 20:01:24 -0700

    Add test cases

--- 118. Pascal Triangle/py/PascalTriangle.py
+++ 118. Pascal Triangle/py/PascalTriangle.py
@@ -6,6 +6,7 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        # LeetCode constraint: 1 <= numRows <= 32
         tree = [[1]]
         for i in range(1, numRows):
             row = [1] + [tree[i-1][j-1] + tree[i-1][j] for j in range(1, i)] + [1]
