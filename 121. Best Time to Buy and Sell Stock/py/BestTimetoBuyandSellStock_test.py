e0349b9a29e50ab7d6b11897c190a7dda7a088e2
César García <ceegee@outlook.com>
2026-03-18 19:09:14 -0700

    Move test cases to a JSON file

--- 121. Best Time to Buy and Sell Stock/py/BestTimetoBuyandSellStock_test.py
+++ 121. Best Time to Buy and Sell Stock/py/BestTimetoBuyandSellStock_test.py
@@ -1,52 +1,15 @@
-import unittest
+import json
+import pathlib
+import pytest
 from BestTimetoBuyandSellStock import Solution
 
 
-class maxProfit_Tests(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test" / "test_cases.json"
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
 
-    def test01(self):
-        self.assertEqual(self.solution.maxProfit([0]), 0)
 
-    def test02(self):
-        self.assertEqual(self.solution.maxProfit([0, 0]), 0)
-
-    def test03(self):
-        self.assertEqual(self.solution.maxProfit([0, 1]), 1)
-
-    def test04(self):
-        self.assertEqual(self.solution.maxProfit([1, 0]), 0)
-
-    def test05(self):
-        self.assertEqual(self.solution.maxProfit([0, 1, 2]), 2)
-
-    def test06(self):
-        self.assertEqual(self.solution.maxProfit([0, 2, 1]), 2)
-
-    def test07(self):
-        self.assertEqual(self.solution.maxProfit([7, 1, 5, 3, 6, 4]), 5)
-
-    def test08(self):
-        self.assertEqual(self.solution.maxProfit([7, 6, 4, 3, 1]), 0)
-
-    def test09(self):
-        self.assertEqual(self.solution.maxProfit([0, 2, 1, 4]), 4)
-
-    def test10(self):
-        self.assertEqual(self.solution.maxProfit([0, 1, 2, 3, 4, 5]), 5)
-
-    def test11(self):
-        self.assertEqual(self.solution.maxProfit([2, 4, 1, 4]), 3)
-
-    def test12(self):
-        self.assertEqual(self.solution.maxProfit([2, 4, 1]), 2)
-
-    def test13(self):
-        self.assertEqual(self.solution.maxProfit([2, 5, 1, 3]), 3)
-
-    def test14(self):
-        self.assertEqual(self.solution.maxProfit([3, 2, 6, 5, 0, 3]), 4)
-
-    def tearDown(self):
-        pass
+@pytest.mark.parametrize('prices, expected', test_cases)
+def test_mergeSortedArray(prices, expected):
+    assert Solution().maxProfit(prices) == expected
