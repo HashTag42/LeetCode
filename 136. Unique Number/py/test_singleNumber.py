a8f536cea1011a73c3ad6fdf9a26b3d5d19697f2
César García <ceegee@outlook.com>
2026-03-21 23:21:10 -0700

    Move test case data to a JSON file

--- 136. Unique Number/py/test_singleNumber.py
+++ 136. Unique Number/py/test_singleNumber.py
@@ -1,15 +1,14 @@
-from singleNumber import Solution
+import json
+import pathlib
 import pytest
+from singleNumber import Solution
+
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test" / "test_cases.json"
+with open(test_cases_path) as f:
+    test_cases = json.load(f)
 
 
-cases = [
-    # nums, expected
-    ([2, 2, 1], 1),
-    ([4, 1, 2, 1, 2], 4),
-    ([1], 1),
-]
-
-
-@pytest.mark.parametrize("nums, expected", cases)
+@pytest.mark.parametrize("nums, expected", test_cases)
 def test_Solution_singleNumber(nums, expected):
     assert Solution().singleNumber(nums) == expected
