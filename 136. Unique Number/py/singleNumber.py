6c8f6f0f06323e70cb5378891aab448333224e46
César García <ceegee@outlook.com>
2026-03-21 23:15:31 -0700

    Refactor after code review

--- 136. Unique Number/py/singleNumber.py
+++ 136. Unique Number/py/singleNumber.py
@@ -2,12 +2,12 @@
 Leet Code problem 136. Unique Number
 https://leetcode.com/problems/single-number/
 '''
-from collections import Counter
 from typing import List
 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        uniques = [item for item, count in counter.items() if count == 1]
-        return uniques[0]
+        result = 0
+        for n in nums:
+            result ^= n
+        return result
