b6eeabd7933e63a9ec5467c91bdd73e34448c36a
César García <ceegee@outlook.com>
2026-03-25 22:29:05 -0700

    Refactor after code review

--- 167. Two Sum II - Input Array Is Sorted/twoSum2.py
+++ 167. Two Sum II - Input Array Is Sorted/twoSum2.py
@@ -3,23 +3,19 @@
     https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 '''
 
-from typing import List
-
 
 class Solution:
-    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum2(self, numbers: list[int], target: int) -> list[int] | None:
         """
         Time complexity: O(n)
-        Space complexity: O(n)
+        Space complexity: O(1)
         """
-        if numbers == [] or numbers is None:
-            length = 0
-        else:
-            length = len(numbers)
-        found_values = dict()
-        for i in range(length):
-            if numbers[i] not in found_values:
-                found_values[numbers[i]] = i
-            wanted = target - numbers[i]
-            if wanted in found_values and i != found_values[wanted]:
-                return [found_values[wanted] + 1, i + 1]
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            s = numbers[left] + numbers[right]
+            if s == target:
+                return [left + 1, right + 1]
+            elif s < target:
+                left += 1
+            else:
+                right -= 1
