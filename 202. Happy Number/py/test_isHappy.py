14ca238f1ba69b9527afce673425cd694cc261ee
César García <ceegee@outlook.com>
2026-04-01 21:21:42 -0700

    Move test case data to a JSON file

--- 202. Happy Number/py/test_isHappy.py
+++ 202. Happy Number/py/test_isHappy.py
@@ -1,15 +1,15 @@
-from isHappy import Solution
+import json
+import pathlib
 import pytest
+from isHappy import Solution
+
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
+with open(test_cases_path) as file:
+    test_cases = json.load(file)
 
 
-isHappy_cases = [
-    (19, True),
-    (2, False),
-]
-
-
-@pytest.mark.parametrize("n, expected", isHappy_cases)
+@pytest.mark.parametrize("n, expected", test_cases)
 def test__isHappy__(n, expected):
     solution = Solution()
     assert solution.isHappy(n) == expected
-
