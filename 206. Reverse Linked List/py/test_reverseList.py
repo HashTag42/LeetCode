4e1ceedd965b1b39ecf7ae731bb76165d039f76f
César García <ceegee@outlook.com>
2026-04-06 23:01:29 -0700

    Move test cases to a JSON file

--- 206. Reverse Linked List/py/test_reverseList.py
+++ 206. Reverse Linked List/py/test_reverseList.py
@@ -1,26 +1,24 @@
-from reverseList import Solution, ListNode
+import json
+import pathlib
 import pytest
+from reverseList import Solution, ListNode
+
+root = pathlib.Path(__file__).resolve().parents[1]
+test_cases_path = root / "test_cases.json"
+with open(test_cases_path) as file:
+    test_cases = json.load(file)
 
 
-cases = [
-    ([], []),
-    ([1, 2], [2, 1]),
-    ([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
-]
-
-
-@pytest.mark.parametrize("nodes, expected", cases)
+@pytest.mark.parametrize("nodes, expected", test_cases)
 def test__reverseList(nodes, expected):
     # Convert input list to a linked list
-    in_head = ListNode(None)
-    current = in_head
-    for i in range(len(nodes)):
-        current.val = nodes[i]
-        if i < len(nodes) - 1:
-            current.next = ListNode()
+    in_head = None
+    if nodes:
+        in_head = ListNode(nodes[0])
+        current = in_head
+        for i in range(1, len(nodes)):
+            current.next = ListNode(nodes[i])
             current = current.next
-
-    in_head = None if in_head.val is None else in_head
     out_head = Solution().reverseList(in_head)
 
     # Conver resulting linked list to a list
