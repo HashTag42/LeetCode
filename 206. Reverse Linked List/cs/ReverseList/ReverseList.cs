75e0e6902a43f2dc4b1f31019e20ba17dcdaf79e
César García <ceegee@outlook.com>
2026-04-07 23:52:16 -0700

    Add C# solution to LeetCode Problem 206. Reverse Linked List

---
+++ 206. Reverse Linked List/cs/ReverseList/ReverseList.cs
@@ -0,0 +1,29 @@
+/*
+LeetCode Problem 206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/
+*/
+
+namespace LeetCode.ReverseList;
+
+ public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public ListNode? ReverseList(ListNode head)
+    {
+        ListNode? prev = null;
+        ListNode? current = head;
+        while (current != null)
+        {
+            ListNode? next_node = current.next;
+            current.next = prev;
+            prev = current;
+            current = next_node;
+        }
+        return prev;
+    }
+}
