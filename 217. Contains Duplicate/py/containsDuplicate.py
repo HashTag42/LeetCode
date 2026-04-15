6ecb3d04840830baea8ec7b2e2bb30c966bdd2e4
César García <ceegee@outlook.com>
2026-04-14 22:49:52 -0700

    Refactor after code review

--- 217. Contains Duplicate/py/containsDuplicate.py
+++ 217. Contains Duplicate/py/containsDuplicate.py
@@ -3,9 +3,8 @@ LeetCode problem 217. Contains Duplicate
 https://leetcode.com/problems/contains-duplicate/
 '''
 from typing import List
-from collections import Counter
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return max(Counter(nums).values()) > 1
+        return len(nums) != len(set(nums))
