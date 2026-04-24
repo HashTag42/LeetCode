58bca376bd4abefddfd6541e60605c47859772a1
César García <ceegee@outlook.com>
2026-04-24 14:19:27 -0700

    Refactor after code review. Move Python files under a 'py' folder

---
+++ 268. Missing Number/py/missingNumber.py
@@ -0,0 +1,33 @@
+'''
+LeetCode problem 268. Missing Number
+https://leetcode.com/problems/missing-number/
+'''
+
+from typing import List
+
+
+class Solution:
+    def missingNumber1(self, nums: List[int]) -> int:
+        """
+        Brute force approach
+        Time complexity = O(n^2)
+        Storage complexity = O(1)
+        """
+        # LeetCode guarantees nums is a valid list with at least one element
+        n = len(nums) + 1
+        for i in range(n):
+            if i not in nums:
+                return i
+        return -1
+
+    def missingNumber2(self, nums: List[int]) -> int:
+        """
+        Optimal approach
+        Time complexity = O(n) to obtain sum_of_elements
+        Storage complexity = O(1)
+        """
+        # LeetCode guarantees nums is a valid list with at least one element
+        n = len(nums)
+        sum_of_elements = sum(nums)
+        actual_sum = (n * (n + 1)) // 2
+        return actual_sum - sum_of_elements
