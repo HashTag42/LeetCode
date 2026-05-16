6e7c53db65b6a3f03f4b6451888f15afd423a2d0
César García <ceegee@outlook.com>
2026-05-15 17:26:29 -0700

    Move test cases to a JSON file

--- 283. Move Zeroes/py/test_moveZeroes.py
+++ 283. Move Zeroes/py/test_moveZeroes.py
@@ -1,15 +1,14 @@
-from moveZeroes import Solution
+import json
+import pathlib
 import pytest
+from moveZeroes import Solution
+
+test_cases_path = pathlib.Path(__file__).resolve().parents[1] / "test_cases.json"
+with open(test_cases_path) as file:
+    test_cases = json.load(file)
 
 
-cases = [
-    ([0], [0]),
-    ([0, 1], [1, 0]),
-    ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
-]
-
-
-@pytest.mark.parametrize("nums, expected", cases)
+@pytest.mark.parametrize("nums, expected", test_cases)
 def test__moveZeroes__(nums, expected):
     solution = Solution()
     solution.moveZeroes(nums)
